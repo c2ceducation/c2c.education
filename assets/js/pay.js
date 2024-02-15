c4a678b07ff4453ad5d04e4ac5a178a3fa3b98c8
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2024-02-15 18:04:57 +0530

    Update the modelpopup for Payment
    Update the modelpopup for Payment

--- assets/js/pay.js
+++ assets/js/pay.js
@@ -4,4 +4,10 @@ pay.addEventListener('click',function(e){
     const val=amount.innerHTML.replace("₹","");
     const c=`http://secure.c2c.education/pay?amount=${val}`;
     pay.setAttribute("href",c);
-})
+});
+const paytwo=document.getElementById('paytwo');
+paytwo.addEventListener('click',function(e){
+    const val=amount.innerHTML.replace("₹","");
+    const c=`http://secure.c2c.education/pay/dplab?amount=${val}`;
+    paytwo.setAttribute("href",c);
+});
